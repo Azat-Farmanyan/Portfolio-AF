cc3b6aec568c4157b322fd44bff844d94af52bcf
Azat <azat.farmanyan2@mail.ru>
2023-08-01 12:08:23 +0400

    feat: create worked with section

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  randomSkillNumber = 0;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.randomSkillNumber = this.getRandomNumber(this.skills.length);
+    }, 2000);
+  }
   skills: String[] = [
     'HTML',
     'CSS',
     'JavaScript',
-    'TypeScript',
     'TypeScript',
     'Angular',
     'VS Code',
@@ -25,4 +31,14 @@ export class AboutComponent {
     'DevExtreme',
     'DevExpress',
   ];
+
+  getRandomNumber(max: number) {
+    // Generate a random floating-point number between 0 (inclusive) and 1 (exclusive)
+    const randomFloat = Math.random();
+
+    // Scale the randomFloat to be between 0 and max (exclusive)
+    const randomNumber = Math.floor(randomFloat * max);
+
+    return randomNumber;
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent, ProjectsComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}

18a7be848556fc84fe0e614def88b3c5d110559f
Azat <azat.farmanyan2@mail.ru>
2023-08-17 11:49:00 +0400

    delete dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
-import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [AppComponent],

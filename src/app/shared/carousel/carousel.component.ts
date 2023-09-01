27d45e07d25d9364e1278d11eb846fdd3f2ab37b
Azat <azat.farmanyan2@mail.ru>
2023-09-01 10:52:31 +0400

    feat: finish carousel

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -1,31 +1,34 @@
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from '@angular/animations';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
-  template: `
-    <ngb-carousel
-      [wrap]="true"
-      [interval]="3000"
-      [pauseOnHover]="true"
-      [pauseOnFocus]="true"
-      [keyboard]="true"
-    >
-      <ng-template ngbSlide *ngFor="let image of images">
-        <div class="picsum-img-wrapper">
-          <img
-            style="
-              width: 100%;
-              height: 70vh;
-              object-fit: contain;"
-            [src]="image"
-            alt="Random first slide"
-          />
-        </div>
-      </ng-template>
-    </ngb-carousel>
-  `,
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'show',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'hide',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('300ms ease-in')),
+    ]),
+  ],
 })
 export class CarouselComponent implements OnChanges {
   @Input() images: string[] | undefined;
@@ -33,33 +36,57 @@ export class CarouselComponent implements OnChanges {
   defaultImg = '';
   activeDot = 0;
   imgLength = 0;
+  showImage: boolean = true;
+  animationTime = 0;
 
   ngOnChanges(): void {
     this.setActiveImg();
     this.setImgsLength();
   }
 
+  toggleImage() {
+    this.showImage = !this.showImage;
+
+    setTimeout(() => {
+      this.showImage = !this.showImage;
+    }, 300);
+  }
+
   setActiveImg() {
-    if (!!this.images) this.defaultImg = this.images[this.activeDot];
+    setTimeout(() => {
+      if (!!this.images) this.defaultImg = this.images[this.activeDot];
+
+      console.log(this.activeDot);
+    }, this.animationTime);
   }
 
   setImgsLength() {
     if (!!this.images) this.imgLength = this.images.length;
+    this.animationTime = 300;
   }
 
   clickRight() {
     this.activeDot++;
     if (this.activeDot === this.imgLength) this.activeDot = 0;
+    this.toggleImage();
     this.setActiveImg();
   }
   clickLeft() {
     this.activeDot--;
     if (this.activeDot < 0) this.activeDot = this.imgLength - 1;
+    this.toggleImage();
     this.setActiveImg();
   }
 
   checkLengthOfImages() {
     if (this.images) {
     }
+  }
+
+  setActiveDot(dotIndex: number) {
+    this.activeDot = dotIndex;
+    this.toggleImage();
+
+    this.setActiveImg();
   }
 }

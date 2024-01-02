774860596f58c9650524a1b7859ea335c516c3e6
Azat Farmanyan <azat.farmanyan2@mail.ru>
2024-01-02 15:09:19 +0400

    feat: add website to tools

--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 export interface Skill {
   name: string;
   img: string;
+  website: string;
 }
 
 @Injectable({
@@ -12,24 +13,64 @@ export class SkillsService {
   constructor() {}
 
   private skills: Skill[] = [
-    { name: 'HTML', img: 'html.png' },
-    { name: 'CSS', img: 'css.png' },
-    { name: 'SCSS', img: 'SCSS.png' },
-    { name: 'JavaScript', img: 'javascript.png' },
-    { name: 'TypeScript', img: 'typescript.png' },
-    { name: 'Angular', img: 'angular.png' },
-    { name: 'RxJs', img: 'rxjs.png' },
-    { name: 'Ionic', img: 'ionic.png' },
-    { name: 'DevExtreme', img: 'devextreme.png' },
-    { name: 'GIT', img: 'git.png' },
-    { name: 'GITHUB', img: 'github.png' },
-    { name: 'VS Code', img: 'vscode.png' },
-    { name: 'VisualStudio', img: 'visualstudio.png' },
-    { name: 'Sublime', img: 'sublime.png' },
-    { name: 'Chrome DevTools', img: 'chrome.png' },
-    { name: 'Figma', img: 'figma.png' },
-    { name: 'Photoshop', img: 'photoshop.png' },
-    { name: 'Illustrator', img: 'illustrator.png' },
+    {
+      name: 'HTML',
+      img: 'html.png',
+      website: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5',
+    },
+    { name: 'CSS', img: 'css.png', website: 'https://www.w3.org/TR/CSS/#css' },
+    { name: 'SCSS', img: 'SCSS.png', website: 'https://sass-lang.com/' },
+    {
+      name: 'JavaScript',
+      img: 'javascript.png',
+      website: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    },
+    {
+      name: 'TypeScript',
+      img: 'typescript.png',
+      website: 'https://www.typescriptlang.org/',
+    },
+    { name: 'Angular', img: 'angular.png', website: 'https://angular.dev/' },
+    { name: 'RxJs', img: 'rxjs.png', website: 'https://rxjs.dev/' },
+    { name: 'Ionic', img: 'ionic.png', website: 'https://ionicframework.com/' },
+    {
+      name: 'DevExtreme',
+      img: 'devextreme.png',
+      website: 'https://js.devexpress.com/',
+    },
+    { name: 'GIT', img: 'git.png', website: 'https://git-scm.com/' },
+    { name: 'GITHUB', img: 'github.png', website: 'https://github.com/' },
+    {
+      name: 'VS Code',
+      img: 'vscode.png',
+      website: 'https://code.visualstudio.com/',
+    },
+    {
+      name: 'VisualStudio',
+      img: 'visualstudio.png',
+      website: 'https://visualstudio.microsoft.com/',
+    },
+    {
+      name: 'Sublime',
+      img: 'sublime.png',
+      website: 'https://www.sublimetext.com/',
+    },
+    {
+      name: 'Chrome DevTools',
+      img: 'chrome.png',
+      website: 'https://developer.chrome.com/docs/devtools',
+    },
+    { name: 'Figma', img: 'figma.png', website: 'https://www.figma.com/' },
+    {
+      name: 'Photoshop',
+      img: 'photoshop.png',
+      website: 'https://www.adobe.com/ru/products/photoshop.html',
+    },
+    {
+      name: 'Illustrator',
+      img: 'illustrator.png',
+      website: 'https://www.adobe.com/products/illustrator.html',
+    },
   ];
 
   getSkills() {

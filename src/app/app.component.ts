2518a8350b9156f74864255c10a88beb78e253b5
Azat Farmanyan <azat.farmanyan2@mail.ru>
2025-05-29 17:02:10 +0400

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {
 import { Subscription, filter, pairwise, take, tap } from 'rxjs';
 import { ProjectsService } from './services/projects.service';
 import { TranslateService } from '@ngx-translate/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-root',
@@ -39,9 +40,69 @@ export class AppComponent implements OnInit {
         const previuosPath = event[0].urlAfterRedirects;
         this.routeService.previousPath.next(previuosPath);
       });
+
+    this.sendTestEmail();
   }
 
   ngOnDestroy(): void {
     if (this.routerSubs) this.routerSubs.unsubscribe();
+  }
+
+  sendTestEmail() {
+    fetch('https://ipapi.co/json/')
+      .then((res) => res.json())
+      .then((data) => {
+        const currentTime = new Date().toLocaleString();
+
+        const visitorText = `
+🌐 Уведомление о новом визите на сайт
+
+🕒 Время: ${currentTime}
+
+🌍 IP-адрес: ${data.ip}
+🏙️ Город: ${data.city}
+🗺️ Регион: ${data.region}
+🗺️ Код региона: ${data.region_code}
+🇷🇺 Страна: ${data.country_name}
+🇷🇺 Код страны: ${data.country_code}
+🌍 Континент: ${data.continent_name}
+📮 Почтовый индекс: ${data.postal}
+⏰ Временная зона: ${data.timezone}
+📅 Смещение UTC: ${data.utc_offset}
+📞 Телефонный код страны: ${data.country_calling_code}
+💰 Валюта: ${data.currency}
+🗣️ Языки: ${data.languages}
+🏢 Организация (провайдер): ${data.org}
+🔢 ASN (номер автономной системы): ${data.asn}
+📡 Сеть: ${data.network}
+
+🧭 Браузер пользователя: ${navigator.userAgent}
+🌐 Язык браузера: ${navigator.language}
+🖥️ Платформа: ${navigator.platform}
+⚙️ Кол-во ядер CPU: ${navigator.hardwareConcurrency || 'неизвестно'}
+🧑‍💻 Производитель браузера: ${navigator.vendor}
+
+🔗 Откуда пришёл (Referrer): ${document.referrer || 'неизвестно'}
+  `;
+
+        const templateParams = { message: visitorText };
+
+        emailjs
+          .send(
+            'service_3cxa21c', // Твой сервис
+            'template_jawmbjg', // Твой шаблон (см. ниже)
+            templateParams,
+            'rOMe2slLxXTWyfjfQ' // Твой публичный ключ
+          )
+          .then((res) => {
+            // console.log('Visitor info sent successfully:', res);
+          })
+          .catch((err) => {
+            // console.error('Failed to send visitor info:', err);
+          });
+      })
+      .catch((err) => {
+        console.error('Failed to fetch visitor info:', err);
+      });
   }
 }
